5a1ff829d954f864e92a0ba848bb44ba8d920f04
Markus Nullmeier <mnullmei@ari.uni-heidelberg.de>
2017-02-17 20:38:53 +0100

    further smoc elaborations.

--- pgs_moc.h
+++ pgs_moc.h
@@ -23,14 +23,15 @@
  * 
  */
 
+#define PG_TOAST_PAGE_FRAGMENT 1010
+#define MOC_PAGE_SIZE (PG_TOAST_PAGE_FRAGMENT / sizeof(moc_interval))
+#define HP64_SIZE (sizeof(hpint64))
+
 typedef struct
 {
 	hpint64 first;
 	hpint64 second;
 } moc_interval;
-
-#define MOC_PAGE_SIZE (1010 / sizeof(moc_interval))
-#define HP64_SIZE (sizeof(hpint64))
 
 typedef struct
 {
@@ -38,15 +39,18 @@ typedef struct
 	char	start[HP64_SIZE];
 } moc_tree_entry;
 
-typedef moc_interval moc_leaf_page[MOC_PAGE_SIZE];
+/* das hier ist Mist: typedef moc_interval moc_leaf_page[MOC_PAGE_SIZE]; */
 
 typedef struct
 {
-	char	vl_len_[4];
-	int32	
+	char			vl_len_[4];	/* size of PostgreSQL variable-length data */
+	int32			root_size	/* number of moc_tree_entry items in root page */
+	hpint64			begin;		/* first Healpix index in set */
+	hpint64			end;		/* 1 + (last Healpix index in set) */
+	int32				/* */
 	
 	
-				
+	unsigned short	version;
 } moc_header;
 
 typedef struct
