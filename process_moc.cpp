bb3b71459ddffb582174b32cec31e19dadbb9e2c
Markus Nullmeier <mnullmei@ari.uni-heidelberg.de>
2017-02-20 22:01:51 +0100

    ever more static_cast<> ...

--- process_moc.cpp
+++ process_moc.cpp
@@ -54,12 +54,16 @@ int
 add_to_moc(void* moc_context, long order, hpint64 first, hpint64 last,
 												pgs_error_handler error_out);
 {
+	moc_input* m = static_cast<moc_input*>(moc_context);
+
 	return 0;
 };
 
 int
 get_moc_size(void* moc_context, pgs_error_handler error_out)
 {
+	moc_input* m = static_cast<moc_input*>(moc_context);
+
 	return 0;
 };
 
@@ -67,15 +71,18 @@ int
 create_moc_release_context(void* moc_context, Smoc* moc,
 												pgs_error_handler error_out);
 {
-	moc->version	= 0;
-	moc->order		= 0 /* ... */;
-	moc->first		= 0 /* ... */;	/* first Healpix index in set */
-	moc->last		= 0 /* ... */;	/* 1 + (last Healpix index in set) */
-	moc->area		= 0 /* ... */;
-	moc->end		= 0 /* ... */;	/* 1 + (offset of last interval) */
+	moc_input* m = static_cast<moc_input*>(moc_context);
+
+
+		moc->version	= 0;
+		moc->order		= 0 /* ... */;
+		moc->first		= 0 /* ... */;	/* first Healpix index in set */
+		moc->last		= 0 /* ... */;	/* 1 + (last Healpix index in set) */
+		moc->area		= 0 /* ... */;
+		moc->end		= 0 /* ... */;	/* 1 + (offset of last interval) */
 
 
 	release_moc_context(moc_context, error_out);
-	return 0;
+	return 1;
 };
 
